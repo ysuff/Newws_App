1e1cbf58206983a5cea3cd12d0e91e2e257e4e79
Yusuf Andiç <yusufandic@icloud.com>
2025-08-19 10:16:51 +0300

    first commit

--- lib/core/constansts/app_constants.dart
+++ lib/core/constansts/app_constants.dart
@@ -1,4 +1,4 @@
 class AppConstants {
-  static const String newsApiKey = 'f1fb69d05efc42159726b5f59f50a77d';
+  static const String newsApiKey = '';
   static const String newsApiBaseUrl = 'https://newsapi.org/v2';
 }
